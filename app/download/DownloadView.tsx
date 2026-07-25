115082c7c2fa72f5b90d4caa2e0d165dc809e2f7
John Forfar <john.forfar@gmail.com>
2026-07-25 09:07:10 +0800

    download page v1.0.12

--- app/download/DownloadView.tsx
+++ app/download/DownloadView.tsx
@@ -9,8 +9,8 @@ import { useEffect, useState } from "react";
 
 const APK_URL =
   "https://github.com/gamerplex/gamerplex-downloads/releases/latest/download/Gamerplex.apk";
-const VERSION = "1.0.11";
-const FILE_SHA = "1897620dad64eae722245e753b4f1c17e4707edc152fc71c2c6b0f0cf1a277a6";
+const VERSION = "1.0.12";
+const FILE_SHA = "dfe858461374cbce43d53b4cf7314ef89dc95069d6a75869607ca2543d571e36";
 const CERT_SHA =
   "58:0F:82:51:11:E7:A5:62:A2:A7:C9:93:4D:EF:4C:ED:6D:F7:9D:A1:04:27:4D:98:00:C4:BD:54:F7:08:79:01";
 
